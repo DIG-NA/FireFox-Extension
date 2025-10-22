412b1af6feeb7a800966d45ad479010c9d1d9d24
DIGNA <xoyenterprise@gmail.com>
2025-10-22 21:41:44 +0400

    -  added 2 wiktionary funs 1. returns html 2.returns proper json
    - added helper fun that retrive a specific value by providing it's key

---
+++ wikitionaryfn.js
@@ -0,0 +1,44 @@
+
+// HTML fun
+async function WikitionaryHtmlFn(text) {
+    const link = `https://en.wiktionary.org/w/api.php?action=query&format=json&prop=extracts&titles=${text}`;
+
+    try {
+
+        const response = await fetch(link);
+        if (!response.ok) throw new Error(`HTTP ${response.status}`);
+        const data = await response.json();
+        // const Object = JSON.parse(data);
+
+        // return data["query"]["pages"] || "no avalible data"
+        return findValueByKey(data, "extract")
+
+    } catch (error) {
+
+        console.error("translation failed:", error);
+        return "translation error";
+    }
+
+
+
+}
+
+// API fun
+async function WikitionaryApiFn(text) {
+
+
+}
+
+function findValueByKey(obj, keyToFind) {
+  if (obj.hasOwnProperty(keyToFind)) {
+    return obj[keyToFind];
+  }
+
+  for (const key in obj) {
+    const value = obj[key];
+    if (typeof value === "object" && value !== null) {
+      const result = findValueByKey(value, keyToFind);
+      if (result !== undefined) return result;
+    }
+  }
+}
